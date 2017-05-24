c2fe28f762dbd74a28ee56948fb6963482ed18f0
Heather Johnson <heathermariej@users.noreply.github.com>
2017-05-23 23:32:08 -0700

    dynamically adding callback url

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ end
 post '/auth' do
   url = "https://beam.pro/oauth/authorize"
   url += "?response_type=token"
-  url += "&redirect_uri=http://localhost:4567/callback"
+  url += "&redirect_uri=#{request.base_url}/callback"
   url += "&scope=chat:connect%20chat:chat"
   url += "&client_id=#{ENV['CLIENT_ID']}"
 
